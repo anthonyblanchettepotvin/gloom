360347541b3ec203a457fc04f3ccd3eda1555978
anthonyblanchettepotvin <anthony.blanchette.potvin@gmail.com>
2023-08-20 19:53:24 -0400

    replaced sliders with color edit controls where appropriate

--- gloom/gloom/src/ui/imgui/adapter/ImGuiDirectionalLightComponentAdapter.cpp
+++ gloom/gloom/src/ui/imgui/adapter/ImGuiDirectionalLightComponentAdapter.cpp
@@ -16,8 +16,8 @@ void ImGuiDirectionalLightComponentAdapter::Render() const
 	if (m_DirectionalLightComponent.GetDirectionalLight())
 	{
 		ImGui::InputFloat3("Direction", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetDirectionReference()));
-		ImGui::SliderFloat3("Ambient Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetAmbientColorReference()), 0.0f, 1.0f);
-		ImGui::SliderFloat3("Diffuse Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetDiffuseColorReference()), 0.0f, 1.0f);
-		ImGui::SliderFloat3("Specular Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetSpecularColorReference()), 0.0f, 1.0f);
+		ImGui::ColorEdit3("Ambient Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetAmbientColorReference()));
+		ImGui::ColorEdit3("Diffuse Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetDiffuseColorReference()));
+		ImGui::ColorEdit3("Specular Color", glm::value_ptr(m_DirectionalLightComponent.GetDirectionalLight()->GetSpecularColorReference()));
 	}
 }
