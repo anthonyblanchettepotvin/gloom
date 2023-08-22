b1ef08867c6868b572ae219d71a7047dc8f324dc
anthonyblanchettepotvin <anthony.blanchette.potvin@gmail.com>
2023-08-21 20:05:48 -0400

    fixed an issue where a world could contain multiple references to the same actor

--- gloom/gloom/src/game/world/World.cpp
+++ gloom/gloom/src/game/world/World.cpp
@@ -4,5 +4,13 @@
 
 void World::SpawnActor(Actor& actor)
 {
-	m_Actors.push_back(&actor);
+	auto it = std::find(m_Actors.begin(), m_Actors.end(), &actor);
+	if (it == m_Actors.end())
+	{
+		m_Actors.push_back(&actor);
+	}
+	else
+	{
+		// TODO: Log error
+	}
 }
